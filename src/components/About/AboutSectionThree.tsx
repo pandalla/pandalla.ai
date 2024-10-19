b813a46797ac9c6d88a3195d6204a9f9d035746a
JessyTsu1 <jiaxicui446@gmail.com>
2024-10-19 12:55:08 +0800

    init video

--- src/components/About/AboutSectionThree.tsx
+++ src/components/About/AboutSectionThree.tsx
@@ -40,7 +40,7 @@ const AboutSectionTwo = () => {
               <MetricCard value="84%" description="Efficiency Increase" />
               <MetricCard value="1300+" description="Hours Saved Per Project" />
               <MetricCard value="$52K" description="Average Cost Savings" />
-              <MetricCard value="1.3B+" description="Total Data Records Generated" />
+              <MetricCard value="1.4B+" description="Total Data Records Generated" />
             </div>
           </div>
         </div>
