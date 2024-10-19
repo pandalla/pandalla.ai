4ecdb1e08e687d990732763f42aedeef3e9eb259
JessyTsu1 <jiaxicui446@gmail.com>
2024-10-20 01:08:35 +0800

    bugfix og

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     description: "Elevate your data strategy, safeguard privacy, and accelerate innovation with Pandalla.ai's state-of-the-art synthetic data and AI technologies.",
     images: [
       {
-        url: "https://pandalla.ai/images/og-image.png",
+        url: "https://pandalla.ai/images/opengraph-image.png",
         width: 1200,
         height: 630,
         alt: "Pandalla.ai - Synthetic Data and AI Solutions",
@@ -24,10 +24,10 @@ export const metadata: Metadata = {
     creator: '@PandallaAI',
     title: 'Pandalla.ai | Leading Synthetic Data and AI Solutions',
     description: 'Pandalla.ai offers cutting-edge synthetic data generation and AI technologies to help businesses enhance data quality, protect privacy, and accelerate innovation. Explore our solutions and unlock the power of AI.',
-    images: ["https://pandalla.ai/images/og-image.png"],
+    images: ["https://pandalla.ai/images/twitter-image.png"],
   },
   other: {
-    'feishu:image': 'https://pandalla.ai/images/og-image.png',
+    'feishu:image': 'https://pandalla.ai/images/feishu-image.png',
     'feishu:card': 'summary_large_image',
     'feishu:title': 'Pandalla.ai | Leading Synthetic Data and AI Solutions',
     'feishu:description': 'Pandalla.ai offers cutting-edge synthetic data generation and AI technologies to help businesses enhance data quality, protect privacy, and accelerate innovation. Explore our solutions and unlock the power of AI.',
