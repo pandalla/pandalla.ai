ad04b66bd93823ab1c91d4c7654cc8c82822d887
JessyTsu1 <jiaxicui446@gmail.com>
2024-10-19 21:44:55 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,10 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
-import { Inter } from "next/font/google";
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import "../styles/ursine-theme.css";
-
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -19,20 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-
-      <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
+      <body className="bg-[#FCFCFC] dark:bg-black">
         <Providers>
           <Header />
           {children}
           <Footer />
           <ScrollToTop />
         </Providers>
-
         <SpeedInsights />
       </body>
     </html>
--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -10,7 +10,7 @@ const Video = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className="relative z-10 py-16 md:py-20 lg:py-28">
+    <section className="relative z-10 py-16 md:py-20 lg:py-28 font-inter">
       <div className="container">
         <SectionTitle
           title="AI-Driven Synthetic Video Annotation is coming"
